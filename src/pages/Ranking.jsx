4bd130784a98d30fc18dd6360ebf20174c8d8f6a
Luca Luca <galaxygame83@gmail.com>
2024-11-10 13:14:10 +0100

    add ranking page

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,9 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import Root, { Context } from "../pages/Root";
+import '../assets/Ranking.css'
+import { useTranslation } from "react-i18next";
 
 export default function Ranking(){
     const [teachers, setTeachers] = useState([]);
     const { customFetch } = useContext(Context)
+    const [t] = useTranslation('global')
     
     useEffect(()=>{
         customFetch(`/api/teachers/ranking`, {
@@ -16,20 +19,34 @@ export default function Ranking(){
     
     return (
         <div id="Ranking">
-            {
-                teachers.length > 0 ? (
-                        teachers.map((teacher) => (
-                            <div key={teacher.id}>
-                                <h2>{teacher.name}</h2>
-                                <p>{teacher.rating}</p>
-                            </div>
-                        ))
-                ) : (
-                    <>Loading...</>
-                )
-            }
-
-
+            <h1>
+                {t('Ranking.title')}
+            </h1>
+            <ol>
+                {
+                    teachers.length > 0 ? (
+                            teachers.map((teacher, index) => (
+                                <li key={teacher.id}>
+                                    <h3>{index+1}</h3>
+                                    <h2>{teacher.prefix} {teacher.name}</h2>
+                                    <span>
+                                        {
+                                            teacher.subjects.map((subject,index)=>(
+                                                <p>
+                                                    {subject}
+                                                </p>
+                                            ))
+                                        }
+                                    </span>
+                                    <img src="/Icons/Star.svg" alt="Star" />
+                                    <span>{parseFloat(teacher.rating).toFixed(1)}</span>
+                                </li>
+                            ))
+                    ) : (
+                        <>Loading...</>
+                    )
+                }
+            </ol>
         </div>
     )
 }
